ab3d252a446bf3787d412ab13900fc3abd648821
pawellewarski <pawellewarski@gmail.com>
2018-10-13 15:43:41 +0200

    first commit

---
+++ src/main/java/pl/akademiakodu/demorepo/DemorepoApplication.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu.demorepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemorepoApplication.class, args);
+	}
+}
